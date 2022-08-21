1d0ba47bf33d3a68dd09baeeb1a8749d5f785511
Stoic Citium <vankhoa0603@gmail.com>
2022-08-21 09:17:26 +0700

    add unit

--- defi-contracts/tests/desplitly_test.ts
+++ defi-contracts/tests/desplitly_test.ts
@@ -98,7 +98,7 @@ Clarinet.test({
 
             Tx.contractCall(contractPrincipal(deployer), "update-expense", 
             [types.ascii("expense001"),types.ascii("expense 1 updated"),types.ascii("description"),types.ascii("image.link"),types.ascii("22/02/2022"),types.uint(10000000), types.bool(false)
-        ],wallet1.address)])
+        ],wallet1.address) ])
         
         let [createExpense, updateExpense] = block.receipts;
         createExpense.result.expectOk().expectBool(true);
@@ -152,5 +152,119 @@ Clarinet.test({
         let [createExpense, updateExpense] = block.receipts;
         createExpense.result.expectOk().expectBool(true);
         updateExpense.result.expectErr().expectUint(ERR_NOT_EXPENSE_CREATOR);
+    },
+});
+
+Clarinet.test({
+    name: "[OK]] pay-expense ",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+        let creator = accounts.get("wallet_5")!;
+        let wallet1 = accounts.get("wallet_1")!;
+        let wallet2 = accounts.get("wallet_2")!;
+        let wallet3 = accounts.get("wallet_3")!;
+        
+        const expenseID = "expense001";
+        const OWNED_AMOUNT = 1000000
+        
+        let block = chain.mineBlock([
+            // wallet 5 creates an expense
+            Tx.contractCall(contractPrincipal(deployer), "create-expense", 
+            [types.ascii(expenseID),types.ascii("expense 1"),types.ascii("description"),types.ascii("image.link"),types.ascii("22/02/2022"),types.uint(10000000), 
+            types.list([types.tuple({"sharer": types.principal(wallet1.address), "owned-amount": types.uint(OWNED_AMOUNT) }), 
+            types.tuple({"sharer": types.principal(wallet2.address), "owned-amount": types.uint(5000000)  }),
+            types.tuple({"sharer": types.principal(wallet3.address), "owned-amount": types.uint(4000000)  })
+        ])], 
+        creator.address),
+
+            // wallet 1 pays an expense
+            Tx.contractCall(contractPrincipal(deployer), "pay-expense", 
+            [types.ascii(expenseID),types.principal(creator.address)
+        ],wallet1.address)])
+        
+        let [createExpense, payExpense] = block.receipts;
+        createExpense.result.expectOk().expectBool(true);
+        payExpense.result.expectOk().expectBool(true);
+        const event = payExpense.events[0].stx_transfer_event
+        assertEquals(Number(event.amount),OWNED_AMOUNT)
+        assertEquals(event.sender,wallet1.address)
+        assertEquals(event.recipient,creator.address)
+    },
+});
+Clarinet.test({
+    name: "[FAIL]] pay-expense | expense is inactive",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+        let creator = accounts.get("wallet_5")!;
+        let wallet1 = accounts.get("wallet_1")!;
+        let wallet2 = accounts.get("wallet_2")!;
+        let wallet3 = accounts.get("wallet_3")!;
+        
+        const expenseID = "expense001";
+        const OWNED_AMOUNT = 1000000
+        
+        let block = chain.mineBlock([
+            // wallet 5 creates an expense
+            Tx.contractCall(contractPrincipal(deployer), "create-expense", 
+            [types.ascii(expenseID),types.ascii("expense 1"),types.ascii("description"),types.ascii("image.link"),types.ascii("22/02/2022"),types.uint(10000000), 
+            types.list([types.tuple({"sharer": types.principal(wallet1.address), "owned-amount": types.uint(OWNED_AMOUNT) }), 
+            types.tuple({"sharer": types.principal(wallet2.address), "owned-amount": types.uint(5000000)  }),
+            types.tuple({"sharer": types.principal(wallet3.address), "owned-amount": types.uint(4000000)  })
+        ])], 
+        creator.address),
+
+            // creator changes expense's status to false
+            Tx.contractCall(contractPrincipal(deployer), "update-expense", 
+            [types.ascii("expense001"),types.ascii("expense 1 updated"),types.ascii("description"),types.ascii("image.link"),types.ascii("22/02/2022"),types.uint(10000000), types.bool(false)
+        ],creator.address),
+
+            // wallet 1 pays an expense
+            Tx.contractCall(contractPrincipal(deployer), "pay-expense", 
+            [types.ascii(expenseID),types.principal(creator.address)],wallet1.address)
+    
+    ])
+        
+        let [createExpense, updateExpense, payExpenseAgain] = block.receipts;
+        createExpense.result.expectOk().expectBool(true);
+        updateExpense.result.expectOk().expectBool(true);
+        payExpenseAgain.result.expectErr().expectUint(ERR_EXPENSE_INACTIVE)
+    },
+});
+Clarinet.test({
+    name: "[FAIL]] pay-expense | already paid for this expense",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        let deployer = accounts.get("deployer")!;
+        let creator = accounts.get("wallet_5")!;
+        let wallet1 = accounts.get("wallet_1")!;
+        let wallet2 = accounts.get("wallet_2")!;
+        let wallet3 = accounts.get("wallet_3")!;
+        
+        const expenseID = "expense001";
+        const OWNED_AMOUNT = 1000000
+        
+        let block = chain.mineBlock([
+            // wallet 5 creates an expense
+            Tx.contractCall(contractPrincipal(deployer), "create-expense", 
+            [types.ascii(expenseID),types.ascii("expense 1"),types.ascii("description"),types.ascii("image.link"),types.ascii("22/02/2022"),types.uint(10000000), 
+            types.list([types.tuple({"sharer": types.principal(wallet1.address), "owned-amount": types.uint(OWNED_AMOUNT) }), 
+            types.tuple({"sharer": types.principal(wallet2.address), "owned-amount": types.uint(5000000)  }),
+            types.tuple({"sharer": types.principal(wallet3.address), "owned-amount": types.uint(4000000)  })
+        ])], 
+        creator.address),
+
+            // wallet 1 pays an expense
+            Tx.contractCall(contractPrincipal(deployer), "pay-expense", 
+            [types.ascii(expenseID),types.principal(creator.address)],wallet1.address),
+
+            // wallet 1 pays an expense again
+            Tx.contractCall(contractPrincipal(deployer), "pay-expense", 
+            [types.ascii(expenseID),types.principal(creator.address)],wallet1.address)
+    
+    ])
+        
+        let [createExpense, payExpense, payExpenseAgain] = block.receipts;
+        createExpense.result.expectOk().expectBool(true);
+        payExpense.result.expectOk().expectBool(true);
+        payExpenseAgain.result.expectErr().expectUint(ERR_ALREADY_PAID_THIS_EXPENSE)
     },
 });
